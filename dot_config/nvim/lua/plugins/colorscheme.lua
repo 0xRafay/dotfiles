ca96a7ee31796f6dcca3530c2c8405a6a1bbbff5
0xRafay <abdulrafay.src@gmail.com>
2026-08-07 01:05:33 -0400

    feat(nvim): restored colorscheme functionality

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,16 @@
 return {
-    "srcery-colors/srcery-vim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd([[colorscheme srcery]])
-    end,
+  'srcery-colors/srcery-vim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd([[colorscheme srcery]])
+
+    local function set_highlight()
+      vim.api.nvim_set_hl(0, 'Search', { bg = '#FAD02C', fg = '#000000', bold = true })
+      vim.api.nvim_set_hl(0, 'CurSearch', { bg = '#FF5733', fg = '#FFFFFF', bold = true })
+      vim.api.nvim_set_hl(0, 'IncSearch', { bg = '#FF5733', fg = '#FFFFFF', bold = true })
+    end
+
+    set_highlight()
+  end,
 }

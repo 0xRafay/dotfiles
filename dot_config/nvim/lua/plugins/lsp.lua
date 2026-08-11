7856653906250f812f84d4de474b4994e00d10a9
0xRafay <abdulrafay.src@gmail.com>
2026-08-11 12:56:54 -0400

    fix: optimize colorizer & nvim-lspconfig startup

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,6 @@
 return {
   'neovim/nvim-lspconfig',
-  event = { 'BufReadPre', 'BufNewFile' },
+  event = { 'VeryLazy' },
   dependencies = {
     'saghen/blink.cmp',
     'williamboman/mason.nvim',
